b969f13a510a62d5eece91bc778a3fea16c95465
Daniel <d.pacek1@gmail.com>
2021-11-26 16:48:36 +0100

    added message errors for form validation

--- mainapp/forms.py
+++ mainapp/forms.py
@@ -7,40 +7,53 @@ from mainapp.models import Donation, Institution, Category
 
 
 class FooterForm(forms.Form):
-    name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={
+    name_errors = {
+        "required": _("Imię jest wymagane"),
+        "max_length": _("Imię jest za długie. Maksymalnie możesz użyć do 100 znaków"),
+        "invalid": _("Proszę podać poprawne imię"),
+    }
+    email_errors = {
+        "required": _("Email jest wymagany"),
+        "max_length": _("Email jest za długi. Maksymalnie możesz użyć do 100 znaków"),
+        "invalid": _("Proszę podać poprawny adres email."),
+    }
+    message_errors = {
+        "required": _("Pole 'Wiadomość' jest wymagane."),
+        "max_length": _("Wiadomość jest za długie. Maksymalnie możesz użyć do 1000 znaków"),
+        "invalid": _("Proszę wprowadź wiadomość."),
+    }
+
+    name = forms.CharField(max_length=100, error_messages=name_errors, widget=forms.TextInput(attrs={
         'type': 'text',
+        'name': 'name',
         'placeholder': 'Imię',
     }))
 
-    surname = forms.CharField(max_length=100, widget=forms.TextInput(attrs={
-        'type': 'text',
-        'placeholder': 'Nazwisko',
+    email = forms.CharField(max_length=100, error_messages=email_errors, widget=forms.TextInput(attrs={
+        'type': 'email',
+        'name': 'email',
+        'placeholder': 'Email',
     }))
 
-    message = forms.CharField(max_length=1000, widget=forms.Textarea(attrs={
+    message = forms.CharField(max_length=1000, error_messages=message_errors, widget=forms.Textarea(attrs={
         'type': 'text',
+        'name': 'message',
         'placeholder': 'Tutaj wpisz wiadomość',
-        'rows': '3',
+        'rows': '2',
         'maxlength': '1000',
     }))
 
     def clean_name(self):
         name = self.cleaned_data['name']
-        if len(name) > 100:
-            raise ValidationError(_('Proszę podać imię które zawiera do 100 znaków'))
         return name[0].upper() + name[1:].lower()
 
     def clean_surname(self):
         surname = self.cleaned_data['surname']
-        if len(surname) > 100:
-            raise ValidationError(_('Proszę podać nazwisko które zawiera do 100 znaków'))
         return surname[0].upper() + surname[1:].lower()
 
-    def clean_message(self):
-        message = self.cleaned_data['message']
-        if len(message) > 1000:
-            raise ValidationError(_('Wiadomośc może zawierać do 1000 znaków'))
-        return message
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        return email.lower()
 
 
 class DonationForm(forms.ModelForm):
@@ -51,7 +64,52 @@ class DonationForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(DonationForm, self).__init__(*args, **kwargs)
-
+        self.fields['quantity'].error_messages = {
+            "required": _("Ilość worków jest wymagane"),
+            "max_length": _("Możesz maksymalnie użyć 20 worków"),
+            "invalid": _("Proszę o podanie prawidłowej ilości worków"),
+        }
+        self.fields['address'].error_messages = {
+            "required": _("Adres jest wymagany"),
+            "max_length": _("Adres jest za długi. Maksymalnie możesz użyć do 200 znaków."),
+            "invalid": _("Proszę o podanie prawidłowego adresu"),
+        }
+        self.fields['phone_number'].error_messages = {
+            "required": _("Numer telefonu jest wymagany"),
+            "max_length": _("Numer telefonu jest niepoprawny"),
+            "invalid": _("Proszę o podanie prawidłowego numeru telefonu"),
+        }
+        self.fields['city'].error_messages = {
+            "required": _("Miasto jest wymagane"),
+            "max_length": _("Miasto jest za długie. Maksymalnie możesz użyć do 100 znaków"),
+            "invalid": _("Proszę o podanie prawidłowego miasta"),
+        }
+        self.fields['institution'].error_messages = {
+            "required": _("Organizacja' jest wymagane"),
+            "max_length": _("Organizacja jest za długa"),
+            "invalid": _("Niewłaściwa organizacja"),
+        }
+        self.fields['categories'].error_messages = {
+            "required": _("Przynajmniej jedna kategoria jest wymagana"),
+            "max_length": _("Kategoria jest za długa. Maksymalnie możesz użyć do 60 znaków"),
+            "invalid": _("Niewłaściwa kategoria"),
+        }
+        self.fields['post_code'].error_messages = {
+            "required": _("Kod pocztowy jest wymagany"),
+            "max_length": _("Kod pocztowy powinien miec format XX-XXX"),
+            "invalid": _("Proszę o podanie prawidłowego kodu pocztowego"),
+        }
+        self.fields['pick_up_date'].error_messages = {
+            "required": _("Data jest wymagana"),
+            "invalid": _("Proszę o podanie prawidłowej daty"),
+        }
+        self.fields['pick_up_time'].error_messages = {
+            "required": _("Godzina jest wymagana"),
+            "invalid": _("Proszę o podanie prawidłowej godziny"),
+        }
+        self.fields['pick_up_comment'].error_messages = {
+            "max_length": _("Komentarz jest za długi. Maksymalnie możesz użyć do 500 znaków."),
+        }
         # # Phone number validator
         # PHONE_REGEX = RegexValidator(r'^(\+\d{1,2})?\d{9}$', "Numer telefonu musi mieć format +XXXXXXXXXXX")
         # phone_validators = [PHONE_REGEX],
@@ -64,56 +122,20 @@ class DonationForm(forms.ModelForm):
         # post_code = self.fields['post_code']
         # post_code.validators.append(post_code_validators)
 
-    def clean_quantity(self):
-        quantity = self.cleaned_data['quantity']
-        if quantity < 1:
-            raise ValidationError(_("Proszę podać prawidłową ilość worków"))
-        return quantity
-
-    def clean_address(self):
-        address = self.cleaned_data['address']
-        if len(address) > 200:
-            raise ValidationError(_("Proszę podać poprawny adres"))
-        return address
-
     def clean_phone_number(self):
         phone_number = self.cleaned_data['phone_number']
-        if len(phone_number) > 12:
-            raise ValidationError(_("Proszę podać poprawny number"))
         return phone_number
 
     def clean_city(self):
         city = self.cleaned_data['city']
-        if len(city) > 100:
-            raise ValidationError(_("Proszę podać poprawne miasto"))
         return city[0].upper() + city[1:].lower()
-
-    def clean_post_code(self):
-        post_code = self.cleaned_data['post_code']
-        if len(post_code) > 6:
-            raise ValidationError(_("Proszę podać poprawny kod pocztowy"))
-        return post_code
-
-    def clean_pick_up_date(self):
-        pick_up_date = self.cleaned_data['pick_up_date']
-        return pick_up_date
-
-    def clean_pick_up_time(self):
-        pick_up_time = self.cleaned_data['pick_up_time']
-        return pick_up_time
-
-    def clean_pick_up_comment(self):
-        comment = self.cleaned_data['pick_up_comment']
-        if len(comment) > 500:
-            raise ValidationError(_("Maksymalnie możesz wpisać 500 znaków w uwagach dla kuriera"))
-        return comment
 
     def clean_institution(self):
         institution = self.cleaned_data['institution']
         try:
             institution = Institution.objects.get(pk=institution.pk)
         except Institution.DoesNotExist:
-            raise ValidationError(_("Proszę wybrać poprawną instytucje"))
+            raise ValidationError(_("Proszę wybrać poprawną instytucję."))
         return institution
 
     def clean_categories(self):
@@ -122,5 +144,5 @@ class DonationForm(forms.ModelForm):
             try:
                 category = Category.objects.get(pk=category.pk)
             except Category.DoesNotExist:
-                raise ValidationError(_("Proszę wybrać poprawną kategorie"))
+                raise ValidationError(_("Proszę wybrać poprawną kategorię."))
         return categories
